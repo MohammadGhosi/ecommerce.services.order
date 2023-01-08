dc51b7c4f6639b9033860de932074cdda94df10e
mohammad.development <mohammad.development@gmail.com>
2023-01-08 13:50:32 +0530

    Project setup

---
+++ ecommerce.services.order/ecommerce.services.order/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using ecommerce.services.order.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce.services.order.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        { }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public async Task<IActionResult> PostAsync([FromBody] User user)
+        {
+           
+            return Created("",user);
+        }
+    }
+}
